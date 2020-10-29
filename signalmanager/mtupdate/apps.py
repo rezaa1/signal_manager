005d81df95392eb914cc549d75943ac9378331d5
U-LAPTOP-P3O47OGB\reza <reza@labtop.com>
2020-10-29 23:45:45 +1100

    Add standard_symbol in mtupdate so it will replace instandard symbols and can be obtained from mt4

--- signalmanager/mtupdate/apps.py
+++ signalmanager/mtupdate/apps.py
@@ -5,11 +5,8 @@ class MtupdateConfig(AppConfig):
     name = 'mtupdate'
 
 
-<<<<<<< HEAD
 def get_standard_symbol(symbol):
-=======
-def manage_trades(symbol):
->>>>>>> 7062faf0cd29a3a2820217b3eb541fe0d600852d
+
 	rsymbol = symbol.split('-e')[0]
 
 	return(rsymbol)
