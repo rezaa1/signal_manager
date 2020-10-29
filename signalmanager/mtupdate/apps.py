31a95d13664d73c7f552e4bd462b72d9ae4470b2
U-LAPTOP-P3O47OGB\reza <reza@labtop.com>
2020-10-29 23:10:40 +1100

    Add standard_symbol in mtupdate so it will replace instandard symbols and can be obtained from mt4

--- signalmanager/mtupdate/apps.py
+++ signalmanager/mtupdate/apps.py
@@ -3,3 +3,11 @@ from django.apps import AppConfig
 
 class MtupdateConfig(AppConfig):
     name = 'mtupdate'
+
+
+def get_standard_symbol(symbol):
+	rsymbol = symbol.split('-e')[0]
+
+	return(rsymbol)
+
+
