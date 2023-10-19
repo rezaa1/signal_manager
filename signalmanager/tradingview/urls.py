f8d5a3b9a8dd2699242ce0a08365f54e596d5921
reza_azimi <reza.azimi@inferencesolutions.com>
2023-10-19 18:19:18 +1100

    import api_view

--- signalmanager/tradingview/urls.py
+++ signalmanager/tradingview/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from tradingview import views
 
-from rest_framework.decorators import permission_classes, authentication_classes
+from rest_framework.decorators import permission_classes, authentication_classes, api_view
 from rest_framework.permissions import AllowAny
 
 
